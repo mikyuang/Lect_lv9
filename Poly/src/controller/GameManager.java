214dac20f6f95b255a55a5771a616b40a0fb1962
mikyuangkim <mikyuangkim@localhost>
2021-10-27 18:07:45 +0900

    Poly

---
+++ Poly/src/controller/GameManager.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class GameManager {
+
+	public GameManager() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ Poly/src/controller/UnitManager.java
@@ -0,0 +1,9 @@
+package controller;
+
+public class UnitManager {
+
+	public UnitManager() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
